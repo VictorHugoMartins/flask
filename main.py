a74238a1a202bd019f5abb4e17d3a4c53482baf9
Victor Hugo Martins <62341427+VictorHugoMartins@users.noreply.github.com>
2023-06-02 19:49:49 -0300

    headers de cors

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app = Flask(__name__) # Dados de usuário armazenados em um dicionário
 
 @app.route('/')
 def index():
-  return jsonify({"message": "Erro ao retornar colunas para seleção de dados para coleta!", "success": False}), 500 # Inicia a aplicação
+	response = jsonify({"message": "Erro ao retornar colunas para seleção de dados para coleta!", "success": False}), 500 # Inicia a aplicação
+	response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
 
 @app.route('/a')
 def h(): # Recebe o username e password do request em formato json
