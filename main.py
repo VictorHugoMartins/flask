00a112276c9bda04fb9ac535bdf1cbd4bcc28d1c
Victor Hugo Martins <62341427+VictorHugoMartins@users.noreply.github.com>
2023-06-02 19:31:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ exclusive_airbnb_columns = ['host_id', 'name', 'minstay', 'bathroom', 'avg_ratin
 exclusive_booking_columns = ['start_date', 'finish_date']
 
 app = Flask(__name__) # Dados de usuário armazenados em um dicionário
-CORS(app)
 
 @app.route('/')
-def h(): # Recebe o username e password do request em formato json
-	return jsonify({"message": "Erro ao retornar colunas para seleção de dados para coleta!", "success": False}), 500 # Inicia a aplicação
+def index():
+  return jsonify({"message": "Erro ao retornar colunas para seleção de dados para coleta!", "success": False}), 500 # Inicia a aplicação
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000,debug=True)
+  app.run(port=5000)
