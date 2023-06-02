4d66850cbf8f80ebc986eeeecbfbf60a44f8db11
Victor Hugo Martins <62341427+VictorHugoMartins@users.noreply.github.com>
2023-06-02 19:44:41 -0300

    new test

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-
-exclusive_airbnb_columns = ['host_id', 'name', 'minstay', 'bathroom', 'avg_rating', 'extra_host_languages', 'is_superhost', 'room_type']
-exclusive_booking_columns = ['start_date', 'finish_date']
 
 app = Flask(__name__) # Dados de usuário armazenados em um dicionário
 
 @app.route('/')
 def index():
   return jsonify({"message": "Erro ao retornar colunas para seleção de dados para coleta!", "success": False}), 500 # Inicia a aplicação
+
+@app.route('/a')
+def h(): # Recebe o username e password do request em formato json
+	return "hello"
 
 if __name__ == '__main__':
   app.run(port=5000)
